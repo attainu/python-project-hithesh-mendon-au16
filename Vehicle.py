ab1310602c0eefe34972c5614fe98e4f1d1fb8c7
hithesh-mendon-au16 <74250841+hithesh-mendon-au16@users.noreply.github.com>
2021-02-21 10:37:49 +0530

    added vehicle.py to the folder

---
+++ Vehicle.py
@@ -0,0 +1,12 @@
+class Vehicle:
+	def __init__(self,register_number,color):
+		self.color =  color
+		self.register_number = register_number
+
+class Car(Vehicle):
+
+	def __init__(self,register_number,color):
+		Vehicle.__init__(self,register_number,color)
+
+	#def getType(self):
+	#	return "Car"
